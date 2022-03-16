26e818339ec9f8f694a6e9813d30ef44411a3530
Atharva Taranekar <taranekaratharva@gmail.com>
2022-03-17 01:33:56 +0530

    first commit

---
+++ src/Components/header/header.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export const Header = ()=>{
+
+    return(
+        <div className = "header-container">
+            <img src="G:\projects\React\infeedo\public\assets\amber.png" alt="amber" />
+            <h1>Amber</h1>
+        </div>
+    )
+}
---
+++ src/Pages/ChatPage/chatPage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header } from '../../Components/header/header.component';
+export const chatpage = ()=> {
+
+    return(
+        <div>
+            <Header/>
+        </div>
+    )
+}
